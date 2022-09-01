0171856bfe768b522c90e902cd944c26e4e65cfb
saiyuki603 <4120254s@g.hit-u.ac.jp>
2022-09-01 15:08:33 +0900

    B4

--- B4.py
+++ B4.py
@@ -10,30 +10,38 @@ import time
 
 import os
 
-URL = 'http://www.geocoding.jp/api/'
-
-def coordinate(address):
+def saitama_doro(address_list):
+    err = 0
     """
     addressに住所を指定すると緯度経度を返す。
-
     >>> coordinate('東京都文京区本郷7-3-1')
     ['35.712056', '139.762775']
     """
+    URL = 'http://www.geocoding.jp/api/'
+
+    address = "-".join(address_list)
+
     payload = {'q': address}
     xml = requests.get(URL, params=payload)
     soup = BeautifulSoup(xml.content, "xml")
     if soup.find('error'):
-        raise ValueError(f"Invalid address submitted. {address}")
+        err = 1
     latitude = soup.find('lat').string
     longitude = soup.find('lng').string
 
+    soup = BeautifulSoup(xml.content, 'xml')
+    found = soup.find('google_maps').string
+    
+    if found[-1] != '0' or found[-1] != '1' or found[-1] != '2' or found[-1] != '3' or found[-1] != '4' or found[-1] != '5' or found[-1] != '6' or found[-1] != '7' or found[-1] != '8' or found[-1] != '9':
+        err = 1
+
     """
-    埼玉の下水を検索・スクショ
+    埼玉の道路を検索・スクショ
     """
-    gesui_url = 'https://www.sonicweb-asp.jp/saitama_g/map?theme=th_90#scale=500&pos=' + longitude + ',' + latitude
+    doro_url = 'https://www.sonicweb-asp.jp/saitama_g/map?theme=th_31#scale=1000#pos=' + longitude + ',' + latitude
     driver = webdriver.Chrome()
 
-    driver.get(gesui_url)
+    driver.get(doro_url)
     time.sleep(5)
 
     iframe = driver.find_element(By.XPATH, '//*[@id="agreement_mask"]')
@@ -48,11 +56,15 @@ def coordinate(address):
     同意画面クリック
     """
     time.sleep(5)
-    driver.find_element(By.XPATH, '//*[@id="footer"]/div[1]/a[21]').click()
+    driver.find_element(By.XPATH, '//*[@id="footer"]/div[1]/a[18]').click()
     """
     縮尺変更
     """
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="side_menu_toggle_btn"]/div[1]').click()
+
     time.sleep(5)
-    FILENAME = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'image/B-3.png')
+    FILENAME = os.path.join(os.path.abspath(os.path.dirname(__file__)), "image\B-4.png")
 
     driver.save_screenshot(FILENAME)
+    return(err)

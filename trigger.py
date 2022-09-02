fc56b040b0334073f204e461ab485d958f660571
Higa Shun <higashun9@gmail.com>
2022-09-02 10:18:42 +0900

    引数なしでも実行できるように改良

--- trigger.py
+++ trigger.py
@@ -174,10 +174,10 @@ def main(query="is:unread", tag="daily_report", count=3):
 # プログラム実行部分
 
 if __name__ == "__main__":
-    #arguments = docopt(__doc__, version="0.1")
-    query = "is:unread"#arguments["<query>"]
-    tag = "map"#arguments["<tag>"]
-    count = 1#arguments["<count>"]
+    # arguments = docopt(__doc__, version="0.1")
+    query = "is:unread"  # arguments["<query>"]
+    tag = "map"  # arguments["<tag>"]
+    count = 1  # arguments["<count>"]
     logging.basicConfig(level=logging.DEBUG)
 
     messages_ = main(query=query, tag=tag, count=count)
@@ -189,8 +189,8 @@ if __name__ == "__main__":
         re_body_1 = r'"body": ".*, "subject"'
         address = re.search(re_body_1, messages_).group()
         address = address[9:-16]
-        print(address)
+        # print(address)
         re_body_2 = r'<.*@.*>'
         from_mail = re.search(re_body_2, messages_).group()
         from_mail = from_mail[1:-1]
-        print(from_mail)
+        # print(from_mail)

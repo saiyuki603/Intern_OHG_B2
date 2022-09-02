aeb81f67c2b9925a71e90f905e5c7983f261ac9a
Higa Shun <higashun9@gmail.com>
2022-09-02 15:02:23 +0900

    trigger.pyテスト完了

--- trigger.py
+++ trigger.py
@@ -189,8 +189,8 @@ if __name__ == "__main__":
         re_body_1 = r'"body": ".*, "subject"'
         address = re.search(re_body_1, messages_).group()
         address = address[9:-16]
-        # print(address)
+        print(address)
         re_body_2 = r'<.*@.*>'
         from_mail = re.search(re_body_2, messages_).group()
         from_mail = from_mail[1:-1]
-        # print(from_mail)
+        print(from_mail)

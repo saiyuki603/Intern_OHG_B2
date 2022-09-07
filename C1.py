f4dde75b080da56dd4b0edcb520a299e4798e3b0
saiyuki603 <4120254s@g.hit-u.ac.jp>
2022-09-07 10:56:15 +0900

    コードの改善

--- C1.py
+++ C1.py
@@ -1,144 +1,126 @@
 from ctypes import addressof
 import importlib
 import traceback
-import slackweb
+# import slackweb
 from PIL import Image
 import base64
 from email.mime.multipart import MIMEMultipart
 import os
 import PyPDF2
 import sendmail
-import requests
-import json
 
-# エラーログ
-def send_error_log(message):
-        requests.post(
-            "https://hooks.slack.com/services/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",
-            headers={'content-type':'application/json'},
-            data=json.dumps({"text":"no_entry_sign:" + message})
-        )
+address=["千葉市","稲毛区","稲毛","３","７"]
 
-def mail(address,err):
-    sender = "intern.ohg.24b@gmail.com"
-    to ="lidanyang633@gmail.com"
-    merger = PyPDF2.PdfFileMerger()  
+err = 0
+
+merger = PyPDF2.PdfFileMerger()
+
+
+def pdfChanger(imagePath, pdfPath):
+    image = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), imagePath))
+    image1 = image.convert("RGB")
+    PdfPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), pdfPath)
+    image1.save(PdfPath)
+    merger.append(PdfPath)
+
+
+def mail(address_list, err, to):
+    sender = "intern.summer.24b@gmail.com"
+
+    # slack = slackweb.Slack(url = "https://hooks.slack.com/services/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")
 
     # 両方とも取れない
     if err == 1:
-        msg = open('err1.html', 'r', encoding='UTF-8')
-        message_text = msg.read()
-        msg.close()
+        msg = "<font size='6px'>取得範囲外又は誤った住所入力、あるいはシステム上のエラーの疑いがあります。</font>"+"<br>住所をもう一度ご確認の上、再度お試しください。それでも取得できない場合はお問い合わせください。"
+        message_text = msg
 
 
-        subject = "エラーが発生：入力不正又は範囲外"
+        subject = "エラー：データ取得失敗"
         sendmail.create_message(sender, to, subject, message_text, cc=None) 
         attach_file_path = None
         sendmail.main(sender, to, subject, message_text, attach_file_path, cc=None)
-        
-        
-        
+        # slack.notify(text="エラーが発生：入力不正又は範囲外")
 
     elif err == 2:
         # 千葉道路だけとれる
-        Chiba_Doro = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'image/B2.png'))
-        CG2 = Chiba_Doro.convert("RGB")
-        pdfPath2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/CD.pdf')
-        CG2.save(pdfPath2)
-        merger.append(pdfPath2)
+        pdfChanger('image/B2.png', 'pdf/CD.pdf')
 
-        msg = open('err2.html', 'r', encoding='UTF-8')
-        message_text = msg.read()
-        msg.close()
-        subject = "エラーが発生：一部のデータを取得できず"
-        file_path = pdfPath2
+        msg = "<h3>● 道路情報</h3>" + "<font size='6px'>※道路情報を取得できませんでした。</font>" + "<br>" + "<font size='4px'><span style='color: red;'>入力内容の見直し</span>をお願いいたします。</font>" + "<br>" + "<h3>● 下水情報</h3>" # open('err2.html', 'r', encoding='UTF-8')
+        message_text = msg # msg.read()
+        # msg.close()
+        subject = "エラー：一部データ取得失敗"
+        file_path = pdfChanger.PdfPath
         attach_file_path = file_path
         sendmail.create_message_with_attachment(sender, to, subject, message_text, file_path, cc=None)
         sendmail.main(sender, to, subject, message_text, attach_file_path, cc=None)
 
-        
+        # slack.notify(text="エラーが発生：一部のデータを取得できず")
     elif err == 3:
         # 千葉下水だけとれる
-        Chiba_Gesui = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'image/B1.png'))
-        CG1 = Chiba_Gesui.convert("RGB")
-        pdfPath1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/CG.pdf')
-        CG1.save(pdfPath1)
-        merger.append(pdfPath1)
+        pdfChanger('image/B1.png', 'pdf/CG.pdf')
 
-        msg = open('err3.html', 'r', encoding='UTF-8')
-        message_text = msg.read()
-        msg.close()
-        subject = "エラーが発生：一部のデータを取得できず"
-        file_path = pdfPath1
+        msg = "<h3>● 下水情報</h3>" + "<font size='6px'>※下水情報を取得できませんでした。</font>" + "<br>" + "<font size='4px'><span style='color: red;'>入力内容の見直し</span>をお願いいたします。</font>" + "<br>" + "<h3>● 道路情報</h3>"
+        message_text = msg
+        # msg.close()
+        subject = "エラー：一部データ取得失敗"
+        file_path = pdfChanger.PdfPath
         attach_file_path = file_path
         sendmail.create_message_with_attachment(sender, to, subject, message_text, file_path, cc=None)
         sendmail.main(sender, to, subject, message_text, attach_file_path, cc=None)
-        
+        # slack.notify(text="エラーが発生：一部のデータを取得できず")
 
         
 
     # 両方とも取れる
     elif err == 0:
-        if "千葉市" in address[0]:
-            Chiba_Gesui = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'image/B1.png'))
-            CG1 = Chiba_Gesui.convert("RGB")
-            pdfPath1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/CG.pdf')
-            CG1.save(pdfPath1)
 
-            Chiba_Doro = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'image/B2.png'))
-            CG2 = Chiba_Doro.convert("RGB")
-            pdfPath2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/CD.pdf')
-            CG2.save(pdfPath2)
+        address = ''
+        for i in range(3):
+            address = address + address_list[i]
+        for i in range(3, len(address_list)):
+            if i == 3:
+                address = address + address_list[i]
+            else:
+                address = address + '-' + address_list[i]
 
-            merger.append(pdfPath1)
-            merger.append(pdfPath2)
+        if "千葉市" in address_list[0]:
 
-            pdfPath5 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/info.pdf')
+            pdfChanger('image/B1.png', 'pdf/CG.pdf')
+            pdfChanger('image/B2.png', 'pdf/CD.pdf')
 
-            merger.write(pdfPath5)
+            pdfPath1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/info.pdf')
 
-            msg = open('ok.html', 'r', encoding='UTF-8')
-            message_text = msg.read()
-            msg.close()
+            merger.write(pdfPath1)
+
+            msg = str(address) + "<br>" + "<h3>● 道路・下水情報</h3>" + "<font size='6px'>" # open('ok.html', 'r', encoding='UTF-8')
+            message_text = msg # msg.read()
+            # msg.close()
             subject = "データ取得成功"
-            file_path = pdfPath5
+            file_path = pdfPath1
             attach_file_path =file_path
             sendmail.create_message_with_attachment(sender, to, subject, message_text, file_path, cc=None)
             sendmail.main(sender, to, subject, message_text, attach_file_path, cc=None)
 
-        elif "さいたま市" in address[0]:
-            Saitama_Gesui = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'image/B3.png'))
-            CG3 = Saitama_Gesui.convert("RGB")
-            pdfPath3 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/SG.pdf')
-            CG3.save(pdfPath3)
+        elif "さいたま市" in address_list[0]:
+            pdfChanger('image/B3.png', 'pdf/SG.pdf')
+            pdfChanger('image/B4.png', 'pdf/SD.pdf')
+            
+            pdfPath2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/info.pdf')
+            merger.write(pdfPath2)
 
-            Saitama_Doro = Image.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'image/B4.png'))
-            CG4 = Saitama_Doro.convert("RGB")
-            pdfPath4 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/SD.pdf')
-            CG4.save(pdfPath4)
-
-            merger.append(pdfPath3)
-            merger.append(pdfPath4)
-
-            pdfPath6 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pdf/info.pdf')
-            merger.write(pdfPath6)
-
-
-            msg = open('ok.html', 'r', encoding='UTF-8')
-            message_text = msg.read()
-            msg.close()
+            msg = str(address) + "<br>" + "<h3>● 道路・下水情報</h3>" + "<font size='6px'>" # open('ok.html', 'r', encoding='UTF-8')
+            message_text = msg # msg.read()
+            # msg.close()
             subject = "データ取得成功"
-            file_path = pdfPath6
+            file_path = pdfPath2
             attach_file_path= file_path
             sendmail.create_message_with_attachment(sender, to, subject, message_text, file_path, cc=None)
             sendmail.main(sender, to, subject, message_text, attach_file_path, cc=None)
 
+
     merger.close()
 
 
-address=["千葉市","稲毛区","稲毛","３","７"]
-
-err = 1
-mail(address, err)
+# mail(address, err, 'lidanyang633@gmail.com')
 # mailに住所を書く
 # 色変更
